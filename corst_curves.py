07d57c67463a7191a68e2e42b57f868b82af7089
DCM_Laptop <dcmay@ualberta.ca>
2022-05-08 01:48:38 -0600

    Cost curves alpha
    Exploitative commit
    
    We have normal carbon credit accounting
    Waiting for transactive carbon credit accounting to be implemented from the problem space into the solution space thanks to inspiring leadership
    
    #emotionalDamage

--- corst_curves.py
+++ corst_curves.py
@@ -8,8 +8,8 @@ min_SD = 1/5
 min_price = 0.07
 max_price = 0.14
 
-def calculate_pricepoint(S=1, D=1, alpha=1/10):
-    ratio = S/D
+def calculate_pricepoint(S=1, D=1, alpha=1/1.8):
+    ratio = D/(S + 1e-5)
 
     delta = max_price - min_price
     shift = alpha*(ratio - 1.0)
@@ -18,3 +18,8 @@ def calculate_pricepoint(S=1, D=1, alpha=1/10):
     price = min_price + np.maximum(0.0, np.minimum(delta, delta * factor ))
 
     return price, ratio
+
+
+def market(bids, asks, pricepoint=0.1):
+    settled = {}
+    return settled
--- load_generation_models.py
+++ load_generation_models.py
@@ -7,8 +7,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def get_house_data(scale_load=1, scale_gen=1):
-    hours = np.arange(0,24)
+def get_house_data(scale_load=1, scale_gen=1, steps=24):
+    hours = np.arange(0,steps)
     normalize_freq_to_24h=2*np.pi/24
     solar_shift_time = -6
     solar = 1.5*np.sin((hours+solar_shift_time)*normalize_freq_to_24h)
@@ -16,14 +16,13 @@ def get_house_data(scale_load=1, scale_gen=1):
 
     base_load = 0.1
     half_day = int(hours[-1]/2)
-    load_shift_time = 3
     load = np.sin((hours+1)*normalize_freq_to_24h)
     load[0:12] = load[0:12]**6
     load[12:] = load[12:]**2
     load[0:half_day] = load[0:half_day] * (1-base_load-0.2)
     load = load + base_load
 
-    return solar*scale_gen, load*scale_gen
+    return solar*scale_gen, load*scale_load
 
 # p_target positive means we're charging
 # p_target negative means we're discharging
@@ -36,7 +35,7 @@ def bess(p_target=10, soc_now=0.1, capacity=100):
 
     soc_now = soc_now + p_possible/capacity
 
-    return soc_now, p_residual
+    return soc_now, p_residual, p_possible
 
 def plot():
     solar, load = get_house_data()
